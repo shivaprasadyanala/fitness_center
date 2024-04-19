313e429f10d79414cd586da1a55d5b922bea639e
shivaprasadyanala <shivaprasadyanala@gmail.com>
2024-04-19 17:17:12 +0530

    added cart component

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, removeFromCart } from 'react'
 
-const Cart = () => {
- <div>cart</div>
+const Cart = ({ services, length, removeFromCart }) => {
+ const [cart, setCart] = useState([])
+
+
+
+ return (
+  <div className="cart">
+   {/* {length} */}
+   <h3>Cart</h3>
+   {length > 0 ?
+    <div>
+     {services.map((service, index) => (
+      <div key={index}>
+       {service}{' '}
+       <button className="removeFromCartButton" onClick={() => removeFromCart(index)}>
+        Remove
+       </button>
+      </div>
+     ))}
+    </div>
+    : (
+     <p>No items in cart</p>
+    )}
+  </div>
+ )
 }
 
 export default Cart
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,25 +1,50 @@
 import React from 'react'
 import data from '../../data.json'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import './services.css'
-const Services = () => {
+import Cart from '../Cart/Cart'
+const Services = ({ randomData, index }) => {
+ const [cartItems, setCartItems] = useState([]);
+
+ const addToCart = (item) => {
+  if (!cartItems.includes(item)) {
+   setCartItems([...cartItems, item]);
+   localStorage.setItem('items', JSON.stringify(cartItems))
+   // Replace cart with the new service
+   alert(`${item} added to cart`);
+  } else {
+   alert(`${item} already in the cart`)
+  }
+
+ };
  const ser_data = data.services
+ const location = useLocation();
+ const removeFromCart = (index) => {
+  const newCartItems = [...cartItems];
+  newCartItems.splice(index, 1);
+  setCartItems(newCartItems);
+ };
  return (
   <div className='serviceswrapper'>
-   <h1>xyz fitness center</h1>
-   <ul className='serviceslist'>
-    {ser_data.map((services) => (
-     <li className='item'>
-      <img src="https://wallpaperaccess.com/full/804876.jpg" width={150} height={150} alt="" />
-      {services.name}
-      <button className="addToCartButton">
-       Add to Cart
-      </button>
-     </li>
+   {cartItems && <Cart length={cartItems.length} services={cartItems} removeFromCart={removeFromCart} />
+   }
 
-    ))}
-   </ul >
+   <div >
+    <h1 className='serviceheader'>{location.state.serviceName}</h1>
+    <ul className='serviceslist'>
+     {location.state.randomData && location.state.randomData.map((service, index) => (
+
+      <li key={index} className='serviceitem'>
+       <p>{service.name}</p>
+       <button className="addToCartButton" onClick={() => addToCart(service.name)}>
+        Add to Cart
+       </button>
+      </li>
+     ))}
+    </ul>
+   </div>
   </div>
-
  )
 }
 
